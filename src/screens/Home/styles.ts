b1180c362c29da8e81d218f15246a8bfa6feccf9
ElieneMontenegro <lila.amaral@hotmail.com>
2021-10-25 22:04:50 -0300

    feat: first setups

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    //ocupar tela inteira
+    flex: 1,
+    //centralizar o texto horizontalmente
+    justifyContent: "center",
+    //centralizar dentro do container (que ocupa tela toda)
+    alignItems: "center",
+  },
+});
