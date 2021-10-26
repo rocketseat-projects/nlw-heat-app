dbf3a31f438f00e4ad5ba9fba9bfc981814dd37c
ElieneMontenegro <lila.amaral@hotmail.com>
2021-10-26 20:18:37 -0300

    feat: header is done!

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
-
 import LogoSvg from "../../assets/logo.svg";
+import { UserPhoto } from "../UserPhoto/index";
 
 export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
 
-      <Text style={styles.logoutText}>Sair</Text>
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+        <UserPhoto imageUri="https://github.com/ElieneMontenegro.png" />
+      </View>
     </View>
   );
 }
